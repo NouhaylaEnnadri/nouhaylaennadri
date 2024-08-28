dd3b7e859558d7264c8bc425d811142efc11010f
noyl <nouhayla.ennadri@usmba.ac.ma>
2024-08-28 03:05:54 +0200

    header design updated

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 const categories = [
@@ -6,9 +7,26 @@ const categories = [
 ];
 
 const Header = () => {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="flex justify-center pt-6 ">
-      <div className="navbar bg-secondary bg-opacity-60 backdrop-blur-md shadow-lg max-w-md w-full rounded-lg">
+    <div className={`w-full flex justify-center pt-6 pb-6 z-50 ${isSticky ? 'fixed top-0 left-0' : 'relative'}`}>
+      <div className="navbar bg-primary bg-opacity-30 backdrop-blur-md shadow-lg max-w-md w-full rounded-lg">
         <div className="flex-1 text-center">
           <a className="btn btn-ghost text-xl">Noyl</a>
         </div>
