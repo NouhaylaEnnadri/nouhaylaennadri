b17917fdda45d7239a2f38d2632da320eb68fbc3
noyl <nouhayla.ennadri@usmba.ac.ma>
2024-09-01 04:05:53 +0200

    postwidget updated to update the page based on the slug

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,6 +1,20 @@
+import { getRecentPosts, getRelatedPosts } from "@/services";
 import React from "react";
+import React, { useState, useEffect } from "react";
 
-const PostWidget = () => {
+const PostWidget = ({ category, slug }) => {
+  const [widgetPosts, setWidgetPosts] = useState([]);
+  useEffect(() => {
+    if (slug) {
+      getRelatedPosts(category, slug).then((result) => {
+        setWidgetPosts(result);
+      });
+    } else {
+      getRecentPosts().then((result) => {
+        setWidgetPosts(result);
+      });
+    }
+  }, [slug]);
   return <div></div>;
 };
 
