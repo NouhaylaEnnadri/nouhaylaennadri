ac0078bc285945011dd7694ed9b31b13ccca5ffd
noyl <nouhayla.ennadri@usmba.ac.ma>
2024-09-02 04:08:50 +0200

    author image fixed

--- components/Author.jsx
+++ components/Author.jsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 const Author = ({ author }) => (
   <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
     <div className="absolute left-0 right-0 -top-14">
-      <img
-        
+      <Image
+        unoptimized
         alt={author.name}
-        height="100px"
-        width="100px"
+        height={100}  // Remove "px" and use just the number
+        width={100}   // Remove "px" and use just the number
         className="align-middle rounded-full"
         src={author.photo.url}
       />
