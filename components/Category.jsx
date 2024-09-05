4515e383b3c65a355fceaa4d5f2feabbb6b1cfd5
noyl <nouhayla.ennadri@usmba.ac.ma>
2024-09-05 18:06:40 +0200

    categories updated

--- components/Category.jsx
+++ components/Category.jsx
@@ -1,36 +1,34 @@
 import { getCategory } from "@/services";
 import Link from "next/link";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 const Category = () => {
   const [postCategories, setPostCategories] = useState([]);
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const result = await getCategory();
-        setPostCategories(result);
-      } catch (error) {
-        console.error("Error fetching categories:", error);
-      }
-    };
-
-    fetchCategories();
+  const fetchCategories = useCallback(async () => {
+    try {
+      const result = await getCategory();
+      setPostCategories(result);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
   }, []);
 
+  useEffect(() => {
+    fetchCategories();
+  }, [fetchCategories]);
+
   return (
-    <nav className="flex gap-4 flex-wrap justify-start text-xs border-b border-gray-300 pb-2">
-      {postCategories.map((category, index) => {
-        return (
-          <Link
-            key={category.slug}
-            href={`/category/${category.slug}`}
-            className={`hover:bg-secondary border-secondary bg-secondary bg-opacity-30 text-base-content  inline-block px-2.5 py-0.5 text-xs font-medium rounded-md border transition-colors duration-300  `}
-          >
-            {category.name}
-          </Link>
-        );
-      })}
+    <nav className="flex flex-wrap gap-4 justify-start   pb-2 text-xs">
+      {postCategories.map(({ slug, name }) => (
+        <Link
+          key={slug}
+          href={`/category/${slug}`}
+          className="inline-block  px-2.5 py-1 text-xs font-medium text-base-content bg-secondary bg-opacity-30  border border-secondary rounded-md transition-colors duration-300 hover:bg-secondary"
+        >
+          {name}
+        </Link>
+      ))}
     </nav>
   );
 };
