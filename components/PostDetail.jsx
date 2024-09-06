178212211fa751997d84773b8bc26d184a67eb53
noyl <nouhayla.ennadri@usmba.ac.ma>
2024-09-06 23:18:17 +0200

    //

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -4,6 +4,7 @@ import remarkGfm from "remark-gfm";
 import moment from "moment";
 import dynamic from "next/dynamic";
 import Image from "next/image";
+import Link from "next/link";
 
 // Dynamic import of SyntaxHighlighter for client-side only
 const SyntaxHighlighter = dynamic(() =>
@@ -12,86 +13,87 @@ const SyntaxHighlighter = dynamic(() =>
 
 import materialDark from "react-syntax-highlighter/dist/cjs/styles/prism/material-dark";
 
+const calculateReadingTime = (text) => {
+  const wordsPerMinute = 200; // Average reading speed
+  const words = text.split(/\s/).length;
+  const minutes = words / wordsPerMinute;
+  const roundedMinutes = Math.ceil(minutes);
+  return roundedMinutes;
+};
+
 const PostDetail = ({ post }) => {
+  // Access category (assuming it's an array and you want to display the first one)
+  const category =
+    post.category && post.category.length > 0
+      ? post.category[0].name
+      : "Uncategorized";
+
+  // Calculate reading time
+  const readingTime = calculateReadingTime(post.content);
+
   return (
-    <div className="shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
+    <div className="pb-12 mb-8 mx-28 px-4 lg:px-8">
+      {/* Post Title */}
+      <h1 className="mb-8 mx-24 text-4xl font-bold text-base-content ">
+        {post.title}
+      </h1>
+
+      {/* Category and Date */}
+      <div className="mb-6">
+        {/* Category */}
+        <div className=" mx-24 text-xs mb-2">
+          <Link
+            href={`/category/${post.category[0].id}`}
+            className="inline-block px-2 py-0.5 text-xs font-medium text-base-content bg-accent bg-opacity-20 border border-accent rounded-md transition-colors duration-300 hover:bg-accent"
+          >
+            {category}
+          </Link>
+        </div>
+        {/* Post Date and Reading Time */}
+        <div className="flex mx-24 text-gray-500 text-sm gap-4">
+          <span>Posted at {moment(post.createdAt).format("MMM DD, YYYY")}</span>
+          <span>• {readingTime} min Read</span>
+        </div>
+      </div>
+
       {/* Featured Image */}
-      <div className="relative overflow-hidden shadow-md mb-6 rounded-t-lg">
+      <div className="mx-24 elative shadow-xlg overflow-hidden mb-12 rounded-lg">
         <Image
-          width={800} 
-          height={400} 
+          width={800}
+          height={400}
           unoptimized
           src={post.featuredImage.url}
           alt={post.title}
-          className="object-cover h-full w-full rounded-t-lg lg:rounded-lg"
+          className="object-cover h-full w-full rounded-lg"
         />
       </div>
 
-      {/* Post Meta and Author Info */}
-      <div className="px-4 lg:px-0">
-        <div className="flex items-center mb-8">
-          {/* Author Information */}
-          <div className="flex items-center">
-            <Image
-              unoptimized
-              alt={post.author.name}
-              width={40}
-              height={40}
-              className="rounded-full mr-4"
-              src={post.author.photo.url}
-            />
-            <p className="text-gray-700 font-medium text-lg">{post.author.name}</p>
-          </div>
-          {/* Post Date */}
-          <div className="ml-auto text-gray-500 flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 mr-1 text-blue-500"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-              />
-            </svg>
-            <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
-          </div>
-        </div>
-
-        {/* Post Title */}
-        <h1 className="mb-8 text-4xl font-bold text-gray-800">{post.title}</h1>
-
-        {/* Markdown Content with Syntax Highlighting */}
-        <ReactMarkdown
-          className="prose lg:prose-xl"
-          remarkPlugins={[remarkGfm]}
-          components={{
-            code({ node, inline, className, children, ...props }) {
-              const match = /language-(\w+)/.exec(className || "");
-              return !inline && match ? (
-                <SyntaxHighlighter
-                  style={materialDark}
-                  language={match[1]}
-                  PreTag="div"
-                  {...props}
-                >
-                  {String(children).replace(/\n$/, "")}
-                </SyntaxHighlighter>
-              ) : (
-                <code className={className} {...props}>
-                  {children}
-                </code>
-              );
-            },
-          }}
-        >
-          {post.content}
-        </ReactMarkdown>
-      </div>
+      {/* Markdown Content with Syntax Highlighting */}
+      <ReactMarkdown
+        className="prose lg:prose-xl mx-auto p-6"
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={materialDark}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {post.content}
+      </ReactMarkdown>
     </div>
   );
 };
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -8,89 +8,44 @@ import {
 } from "@/components";
 import { getPosts, getPostsDetails } from "@/services";
 import Category from "@/components/Category"; // Import the Category component
-import { FaShareAlt, FaCommentAlt } from "react-icons/fa"; // Import icons from react-icons
 
 const PostDetails = ({ post }) => {
   const [newComment, setNewComment] = useState(null);
-  const [showComments, setShowComments] = useState(false);
 
   const handleNewComment = (comment) => {
     setNewComment(comment);
   };
 
-  const toggleComments = () => {
-    setShowComments(!showComments);
-  };
-
   const slug = post.slug;
   const category = post.category;
-  const comments = post.comments || []; // Provide default empty array
-  const commentsCount = comments.length; // Now it's safe to use length
-
-  // Function to calculate reading time
-  const calculateReadingTime = (content) => {
-    const wordsPerMinute = 200; // Average reading speed
-    const textLength = content.split(/\s+/).length;
-    const minutes = Math.ceil(textLength / wordsPerMinute);
-    return minutes;
-  };
-
-  const readingTime = calculateReadingTime(post.content);
 
   return (
     <div className="container mx-auto px-4 lg:px-8 py-12">
-      <div className="max-w-3xl mx-auto">
-        {/* Categories */}
-        <div className="text-center mb-8">
-          <Category />
-        </div>
+      <div className="max-w-6xl mx-auto">
+        {/* Post Title */}
+
+       
 
         {/* Main Post Content */}
-        <div className="mb-12 w-full bg-white shadow-lg rounded-lg p-8">
+        <div className="mb-12">
           <PostDetail post={post} />
-          <div className="text-right text-gray-500 text-sm mt-4">
-            {readingTime} min read
-          </div>
         </div>
 
-        {/* Action Buttons and Comments Section */}
-        <div className="flex flex-col items-center mt-12">
-          {/* Action Buttons */}
-          <div className="flex gap-4 mb-8">
-            <button
-              className="flex items-center px-4 py-2 text-gray-500 hover:text-gray-700 disabled:opacity-50"
-              disabled
-            >
-              <FaShareAlt className="mr-2" />
-              Share
-            </button>
-            <button
-              className="flex items-center px-4 py-2 text-gray-500 hover:text-gray-700"
-              onClick={toggleComments}
-            >
-              <FaCommentAlt className="mr-2" />
-              {commentsCount}
-            </button>
-          </div>
-
+        {/* Flex Container for Comments and Related Posts */}
+        <div className="flex flex-col lg:flex-row gap-8">
           {/* Comments Section */}
-          {showComments && (
-            <div className="w-full lg:w-2/3 p-6 rounded-lg shadow-md bg-white mb-12">
-              <CommentsForm slug={slug} onNewComment={handleNewComment} />
-              <Comments slug={slug} newComment={newComment} />
+          <div className="flex-1 p-6 rounded-lg shadow-md">
+            <CommentsForm slug={slug} onNewComment={handleNewComment} />
+            <Comments slug={slug} newComment={newComment} />
+          </div>
+
+          {/* Related Posts Section */}
+          <div className="w-full lg:w-1/3">
+            <h3 className="text-2xl font-bold mb-6">Related Posts</h3>
+            <div className="flex flex-col gap-4">
+              {/* Render related posts */}
+              <PostWidget slug={post.slug} category={category} />
             </div>
-          )}
-
-          {/* Author Section */}
-          <Author author={post.author} />
-        </div>
-
-        {/* Related Posts Section */}
-        <div className="w-full lg:w-1/3 bg-white shadow-md rounded-lg p-6 mt-12">
-          <h3 className="text-2xl font-bold mb-6">Related Posts</h3>
-          <div className="flex flex-col gap-4">
-            {/* Render related posts */}
-            <PostWidget slug={post.slug} category={category} />
           </div>
         </div>
       </div>
