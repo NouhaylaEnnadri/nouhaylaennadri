2bf045f36dd87fda374e359bab177d28568ce1f9
noyl <nouhayla.ennadri@usmba.ac.ma>
2024-09-06 23:25:22 +0200

    //

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -32,16 +32,16 @@ const PostDetail = ({ post }) => {
   const readingTime = calculateReadingTime(post.content);
 
   return (
-    <div className="pb-12 mb-8 mx-28 px-4 lg:px-8">
+    <div className="pb-12 mb-8 mx-6 sm:mx-6 lg:mx-28 px-0 sm:px-0 lg:px-4">
       {/* Post Title */}
-      <h1 className="mb-8 mx-24 text-4xl font-bold text-base-content ">
+      <h1 className="mb-8 mx-6 sm:mx-6 lg:mx-24 text-4xl font-bold text-base-content">
         {post.title}
       </h1>
 
       {/* Category and Date */}
       <div className="mb-6">
         {/* Category */}
-        <div className=" mx-24 text-xs mb-2">
+        <div className="mx-6 sm:mx-6 lg:mx-24 text-xs mb-2">
           <Link
             href={`/category/${post.category[0].id}`}
             className="inline-block px-2 py-0.5 text-xs font-medium text-base-content bg-accent bg-opacity-20 border border-accent rounded-md transition-colors duration-300 hover:bg-accent"
@@ -50,14 +50,14 @@ const PostDetail = ({ post }) => {
           </Link>
         </div>
         {/* Post Date and Reading Time */}
-        <div className="flex mx-24 text-gray-500 text-sm gap-4">
+        <div className="flex mx-6 sm:mx-6 lg:mx-24 text-gray-500 text-sm gap-4">
           <span>Posted at {moment(post.createdAt).format("MMM DD, YYYY")}</span>
           <span>• {readingTime} min Read</span>
         </div>
       </div>
 
       {/* Featured Image */}
-      <div className="mx-24 elative shadow-xlg overflow-hidden mb-12 rounded-lg">
+      <div className="mx-6 sm:mx-6 lg:mx-24 relative shadow-xlg overflow-hidden mb-12 rounded-lg">
         <Image
           width={800}
           height={400}
@@ -70,7 +70,7 @@ const PostDetail = ({ post }) => {
 
       {/* Markdown Content with Syntax Highlighting */}
       <ReactMarkdown
-        className="prose lg:prose-xl mx-auto p-6"
+        className="prose lg:prose-xl mx-auto p-6 sm:p-6 lg:p-6"
         remarkPlugins={[remarkGfm]}
         components={{
           code({ node, inline, className, children, ...props }) {
