26befa7d472dde06020bb88a088082a30d0b6826
noyl <nouhayla.ennadri@usmba.ac.ma>
2024-09-12 20:45:14 +0200

    //

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../styles/globals.scss"; // Import your global styles
 import { Layout, Preloader } from "../components"; // Import your components
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
@@ -23,6 +24,16 @@ function MyApp({ Component, pageProps }) {
   // Render the main content after loading
   return (
     <Layout>
+      <Head>
+        <title>Noyl</title>
+        <link
+          rel="icon"
+          href={`data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+  <text x="2" y="20" font-size="12" font-family="Arial" fill="black">NOYL</text>
+</svg>
+`}
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
